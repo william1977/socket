35ec6181d50a7ecec7bb66373f175ed267d0d23e
william1977 <william1977@sina.cn>
2012-08-23 17:05:26 +0000

    add

--- tests/socket_test.cpp
+++ tests/socket_test.cpp
@@ -8,9 +8,9 @@ int main()
     _LOGV("Begin test\n");
     
     IPSocket* socket = new IPSocket;
-    socket->setNonBlock();
-
     socket->create();
+    socket->setNonBlock();
+    
     socket->bind(8000);
     socket->listen();
 
